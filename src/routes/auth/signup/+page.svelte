ab4e99bf4fe58a30221b9b92b9136868253d8f09
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-02-07 15:47:54 -0300

    fix: remove non necessary fields

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,43 +1,30 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
 	import Card from '@components/base/Card.svelte';
 	import { api } from '@src/services/api';
 
-	let displayCompanyDetails: boolean = false;
+	let userName = '';
+	let jobPosition = '';
+	let email = '';
+	let phone = '';
+	let password = '';
+	let birthday = '';
 
-	let recipientNaturalPersonName = ''
-	let recipientNaturalPersonPosition = ''
-	let recipientNaturalPersonEmail = ''
-	let recipientNaturalPersonPhone = ''
-	let recipientPassword = ''
-	let recipientNaturalPersonBirthDate = ''
-	let recipientLegalPersonCompanyName = ''
-	let legalPersonCompanyRegistrationNumber = ''
-	let legalPersonCompanyCountryOffice = ''
-	let legalPersonCompanyAddressOffice = ''
-	let recipientAgreements: boolean = false;
-
-	async function signUpRecipient(): Promise<any> {
+	async function signUpUser(): Promise<any> {
 		const payload = {
-			recipientNaturalPersonName,
-			recipientNaturalPersonPosition,
-			recipientNaturalPersonEmail,
-			recipientNaturalPersonPhone,
-			recipientPassword,
-			recipientNaturalPersonBirthDate,
-			recipientLegalPersonCompanyName,
-			legalPersonCompanyRegistrationNumber,
-			legalPersonCompanyCountryOffice,
-			legalPersonCompanyAddressOffice,
-			recipientAgreements,
-		}
+			userName,
+			jobPosition,
+			email,
+			phone,
+			password,
+			birthday
+		};
 
 		try {
 			const request = await api.post('/recipients', payload);
 		} catch (error) {
-			console.log(error);
+			console.error(error);
 		} finally {
-			console.log('Finished.')
+			console.log('Finished.');
 		}
 	}
 </script>
@@ -53,133 +40,60 @@
 					<p class="text-lg font-bold text-blue-600">Primary Contact Details</p>
 					<input
 						type="text"
-						name="recipientNaturalPersonName"
-						id="recipientNaturalPersonName"
-						bind:value={recipientNaturalPersonName}
+						name="userName"
+						id="userName"
+						bind:value={userName}
 						placeholder="Name"
 						class="input-field"
 						required
 					/>
 					<input
 						type="text"
-						name="recipientNaturalPersonPosition"
-						id="recipientNaturalPersonPosition"
-						bind:value={recipientNaturalPersonPosition}
+						name="jobPosition"
+						id="jobPosition"
+						bind:value={jobPosition}
 						placeholder="Position"
 						class="input-field"
 						required
 					/>
 					<input
 						type="email"
-						name="recipientNaturalPersonEmail"
-						id="recipientNaturalPersonEmail"
-						bind:value={recipientNaturalPersonEmail}
+						name="email"
+						id="email"
+						bind:value={email}
 						placeholder="Email address"
 						class="input-field"
 						required
 					/>
 					<input
 						type="phone"
-						name="recipientNaturalPersonPhone"
-						id="recipientNaturalPersonPhone"
-						bind:value={recipientNaturalPersonPhone}
+						name="phone"
+						id="phone"
+						bind:value={phone}
 						placeholder="Contact number"
 						class="input-field"
 						required
 					/>
 					<input
 						type="password"
-						name="recipientPassword"
-						id="recipientPassword"
-						bind:value={recipientPassword}
+						name="password"
+						id="password"
+						bind:value={password}
 						placeholder="Password"
 						class="input-field"
 						required
 					/>
 					<input
 						type="text"
-						name="recipientNaturalPersonBirthDate"
-						id="recipientNaturalPersonBirthDate"
-						bind:value={recipientNaturalPersonBirthDate}
+						name="birthday"
+						id="birthday"
+						bind:value={birthday}
 						placeholder="Date of Birth (YYYY-MM-DD)"
 						class="input-field"
 						required
 					/>
 				</div>
-				<div>
-					<label class="relative inline-flex items-center cursor-pointer">
-						<input
-							type="checkbox"
-							value=""
-							class="sr-only peer"
-							on:click={(_) => !displayCompanyDetails}
-							bind:checked={displayCompanyDetails}
-						/>
-						<div
-							class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
-						/>
-						<span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-							>Show company details</span
-						>
-					</label>
-				</div>
-				{#if displayCompanyDetails}
-					<div transition:fly={{ y: 100, duration: 500 }} class="w-full flex flex-col gap-2">
-						<p class="text-lg font-bold text-blue-600">Company Details</p>
-						<input
-							type="text"
-							name="recipientLegalPersonCompanyName"
-							id="recipientLegalPersonCompanyName"
-							bind:value={recipientLegalPersonCompanyName}
-							placeholder="Company name"
-							class="input-field"
-							required
-						/>
-						<input
-							type="number"
-							name="legalPersonCompanyRegistrationNumber"
-							id="legalPersonCompanyRegistrationNumber"
-							bind:value={legalPersonCompanyRegistrationNumber}
-							placeholder="Company registration number"
-							class="input-field"
-							required
-						/>
-						<input
-							type="text"
-							name="legalPersonCompanyCountryOffice"
-							id="legalPersonCompanyCountryOffice"
-							bind:value={legalPersonCompanyCountryOffice}
-							placeholder="Company Head Office Country"
-							class="input-field"
-							required
-						/>
-						<input
-							type="text"
-							name="legalPersonCompanyAddressOffice"
-							id="legalPersonCompanyAddressOffice"
-							bind:value={legalPersonCompanyAddressOffice}
-							placeholder="Company Address"
-							class="input-field"
-							required
-						/>
-						<div>
-							<input
-								type="checkbox"
-								name="recipientAgreements"
-								id="recipientAgreements"
-								bind:value={recipientAgreements}
-								class="input-field"
-								required
-							/>
-							<label for="recipientAgreements"
-								>I've read and agree with the terms and conditions</label
-							>
-						</div>
-					</div>
-				{/if}
-				<button type="submit" class="button-primary" on:click={(e) => signUpRecipient()}
-					>Sign Up</button
-				>
+				<button type="submit" class="button-primary" on:click={(e) => signUpUser()}>Sign Up</button>
 			</form>
 			<p>
 				Already have an account? <a href="/auth/signin" class="link-primary">Sign in here!</a>
